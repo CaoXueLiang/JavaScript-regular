23ab2de1ddd088375161b254aaf5188c7e3e6f18
CaoXueLiang <2357747972@qq.com>
2022-09-07 16:28:26 +0800

    正则表达式回溯法原理

--- 第三章_正则表达式括号的作用/02_相关案例.js
+++ 第三章_正则表达式括号的作用/02_相关案例.js
@@ -10,5 +10,41 @@ function trim1(str) {
 }
 console.log(trim1("  foobar  "));
 
-// 2.  将每个单词的首字母转换为大写
-const str1 = "my name is epeli";
+// 2. 将每个单词的首字母转换为大写
+function titleize(str) {
+  let lowStr = str.toLocaleLowerCase();
+  return lowStr.replace(/(?:^|\s)\w/g, (e) => {
+    return e.toUpperCase();
+  });
+}
+console.log(titleize("my name is epeli"));
+
+// 3. 驼峰化
+function camelize(str) {
+  return str.replace(/[-_\s]+(\w)/g, (match, c) => {
+    // console.log(`===${match}===${c}`);
+    return c.toUpperCase();
+  });
+}
+console.log(camelize("-moz-transform"));
+console.log(camelize("-moz-transform "));
+
+// 4. 中划线化
+function dasherize(str) {
+  return str
+    .replace(/([A-Z])/g, (match, c) => {
+      return `-${c}`;
+    })
+    .toLocaleLowerCase();
+}
+// => "-moz-transform
+console.log("==dasherize==", dasherize("MozTransform"));
+
+// 5. 匹配成对标签
+// 匹配一个开标签 <[^>]+>，匹配一个闭标签 <\/[^>]+>
+// 但是要求匹配成对标签，那就需要使用反向引用
+// const regex_tag = /<(\w+)>[\d\D]*<\/\1>/;
+const regex_tag = /<([^>]+)>[\d\D]*<\/\1>/;
+console.log("<title>regular expression</title>".match(regex_tag));
+console.log("<p>laoyao bye bye</p>".match(regex_tag));
+console.log("<title>wrong!</p>".match(regex_tag));
